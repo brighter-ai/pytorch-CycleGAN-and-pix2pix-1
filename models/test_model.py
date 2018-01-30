1f17d28c4a31c9e04463f447bfcf227d8a88bfbb
Coastinger <luedders.h@gmail.com>
2018-01-30 17:29:59 +0100

    test fixes

--- models/test_model.py
+++ models/test_model.py
@@ -28,7 +28,7 @@ class TestModel(BaseModel):
     def set_input(self, input):
         # we need to use single_dataset mode
         input_A = input['A']
-        #self.input_A.resize_(input_A.size()).copy_(input_A)
+        #self.input_A.resize_(input_A.size()).copy_(input_A) #resnet
         self.image_paths = input['A_paths']
 
     def test(self):
